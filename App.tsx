60747b1550191792c28af4f4bcaa36b502548849
pablobeernardo <pablobeernardo@gmail.com>
2023-05-31 21:20:44 -0300

    adicionando arquivos

--- App.tsx
+++ App.tsx
@@ -1,12 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
 export default function App() {
+
+  const [soma, setSoma] = useState(0);
+
+  const subtrairContador = () =>{
+    setSoma(soma - 1)
+  };
+
+  const resetarContador = () =>{
+    setSoma(0);
+  };
+  
+  const Contador = () =>{
+    setSoma(soma + 1) 
+  };
+
+ 
+  const [fontCarregada, setFonteCarregada] = useState(false);
+
+  async function loadFonts(){
+      await Font.loadAsync({
+        bellefair_regular: require('./assets/Bellefair/Bellefair-Regular.ttf')
+      });
+    }
+
+  useEffect(() => {
+    loadFonts()
+    .then (() => {
+      setFonteCarregada(true);
+    })
+    .catch((error) =>{
+      console.log('Ocorreu um erro', error);
+    })
+  }, [])
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {fontCarregada?
+      <Text style={{fontFamily:'bellefair_regular' , fontSize:50,}}>Você clicou: {soma} x</Text>
+      :
+      <Text>Não carregou font</Text>   
+      }
+
       <StatusBar style="auto" />
+
+      <View style={styles.button}>
+        <Button title='Adicionar' onPress={Contador}/>
+        <Button title='Retirar' onPress={subtrairContador}/>
+        <Button title='Resetar' onPress={resetarContador}/>
+      </View>
+
     </View>
+    
   );
 }
 
@@ -17,4 +67,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+button: {
+  flexDirection:'row',
+  marginTop: 30,
+  
+  
+  
+
+
+}
+  
+
+
 });
